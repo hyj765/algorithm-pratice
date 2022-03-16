6e69be93e551348fb0530a2a86732f3c983e2c9e
황영준 <hyj765@naver.com>
2022-03-16 22:45:41 +0900

    graph_algorithm dps
    그래프 dps

---
+++ graph_dfs.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool check[100] = { false };
+
+void dfs(vector<vector<int>> graph,int start) {
+	check[start] = true;
+	cout << start << " ";
+	for (int node : graph[start]) {
+		if (!check[node]) {
+			dfs(graph, node);
+		}
+	}
+}
+
+int main() {
+	int n, m, l;
+	cin >> n >> m >> l;
+	vector<vector<int>> graph(m+1);
+	for (int i = 0; i < m; i++) {
+		int src, dst;
+		cin >> src >> dst;
+		graph[src].push_back(dst);
+		graph[dst].push_back(src);
+	}
+	dfs(graph, 1);
+}
